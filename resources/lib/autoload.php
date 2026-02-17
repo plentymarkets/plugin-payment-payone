bb7ae9c8150d284e425d61966b90be88df2d3999
Nopcea Ligia <ligia.nopcea@plentysystems.com>
2026-02-17 13:19:49 +0200

    remove composer.json and add a single PHP autoloader and switching all library entry points to use it.

---
+++ resources/lib/autoload.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Autoloader for PayoneApi library (no composer.json or vendor needed for this namespace).
+ * Dependencies (e.g. Guzzle) are loaded from vendor/ when present (e.g. SDK symlink).
+ */
+
+$payoneApiBaseDir = __DIR__ . '/PayoneApi/';
+
+spl_autoload_register(function ($class) use ($payoneApiBaseDir) {
+    $prefix = 'PayoneApi\\';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relativeClass = substr($class, $len);
+    $file = $payoneApiBaseDir . str_replace('\\', '/', $relativeClass) . '.php';
+    if (is_file($file)) {
+        require $file;
+    }
+});
+
+if (is_file(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
