7132306eb6ba3834a07f2798a03ff3682b92170c
Nopcea Ligia <ligia.nopcea@plentysystems.com>
2026-02-17 13:07:10 +0200

    composer install

---
+++ resources/lib/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c0277c4d05a2d257d17094eae9b893e1b81b9565',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c0277c4d05a2d257d17094eae9b893e1b81b9565',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
